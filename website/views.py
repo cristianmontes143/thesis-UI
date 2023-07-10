71e11e2eae6e9f38a83a2c05f8bd8d0f83687488
cristianmontes143 <crischuychuyz@gmail.com>
2023-07-10 15:04:04 +0800

    UI run

--- website/views.py
+++ website/views.py
@@ -1,17 +1,22 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, redirect
 from .models import db, Thesis
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
+    return render_template('home.html')
+
+
+@views.route('/backend', methods=['GET', 'POST'])
+def backend():
     if request.method == 'POST':
         thesis_title = request.form['thesis_title']
         thesis_abstract = request.form['thesis_abstract']
         thesis = Thesis(title=thesis_title, abstract=thesis_abstract)
         db.session.add(thesis)
         db.session.commit()
-        return 'Thesis saved successfully'
+        flash('Thesis saved successfully')  # Store success message
+        return redirect('/')  # Redirect to home page
 
-    return render_template('home.html')
 

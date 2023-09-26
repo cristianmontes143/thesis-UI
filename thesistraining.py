edafbbb9390455cd689b594a3c4c58bd32fd36c5
cristianmontes143 <crischuychuyz@gmail.com>
2023-09-26 19:37:08 +0800

    thesis coding

--- thesistraining.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from scipy.cluster.hierarchy import linkage, dendrogram
-import pandas as pd
-import seaborn as sns
-from sklearn import linear_model
-
-
-data = pd.read_csv('FinalData.csv')
